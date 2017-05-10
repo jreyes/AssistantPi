78cc592673f02a75bc7d4211492c5df549fc02c1
Martin Kainzbauer <m.kainzbauer@xtools.at>
2017-05-10 12:41:01 +0200

    fixed bug in updater

--- src/scripts/update.sh
+++ src/scripts/update.sh
@@ -5,26 +5,31 @@ read -r -p "This will backup your config.template.yaml and assistant.example.aso
 
 case $go in
     [Yy] )
-		# Backup config files
+		echo ""
+		echo "### Backup config files"
 		cd /opt
 		sudo mkdir AlexaPi-bkp
+		sudo mkdir AlexaPi-bkp/src
 		sudo cp /opt/AlexaPi/src/assistant.example.asoundrc /opt/AlexaPi-bkp/src/assistant.example.asoundrc
 		sudo cp /opt/AlexaPi/src/config.template.yaml /opt/AlexaPi-bkp/src/config.template.yaml
 
-		# Update AssistantPi Core
+		echo ""
+		echo "### Update AssistantPi Core"
 		cd /opt/AlexaPi
 		sudo git checkout -- .
 		sudo git checkout master
 		sudo git pull
 
-		# Update tweaked Google Assistant SDK
+		echo ""
+		echo "### Update tweaked Google Assistant SDK"
 		cd /opt/AlexaPi/src/assistant-sdk-python
 		sudo git checkout -- .
 		sudo git checkout master
 		sudo git pull
 		sudo /opt/AlexaPi/env/bin/python -m pip install --upgrade -e ".[samples]"
 
-		# Reinstall backed up files
+		echo ""
+		echo "### Reinstall backed up files"
 		sudo mv /opt/AlexaPi-bkp/src/assistant.example.asoundrc /opt/AlexaPi/src/assistant.example.asoundrc
 		sudo mv /opt/AlexaPi-bkp/src/config.template.yaml /opt/AlexaPi/src/config.template.yaml
 		sudo rm -rf /opt/AlexaPi-bkp
